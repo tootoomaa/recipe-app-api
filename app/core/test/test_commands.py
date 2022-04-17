58fa34ecedd438af14f568b0c39401e700a3bbf5
tootoomaa <tootoomaa@naver.com>
2022-04-17 21:03:49 +0900

    feat: Configure postgres db

--- app/core/test/test_commands.py
+++ app/core/test/test_commands.py
@@ -10,11 +10,12 @@ class CommandTests(TestCase):
         # ConnectionHandler를 오버라이드 하여 Mock과 같이 동작하도록 만든다.
         with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
             gi.return_value = True                  # 리턴 값을 덮어 씌움
-            call_command('wait_for_db')             #
-            self.assertEqual(gi.call_count, 1)      #
+            call_command('wait_for_db')
+            self.assertEqual(gi.call_count, 1)
 
     # 5번 까지는 실패, 6번 부터는 정상 실행 확인
-    @patch('time.sleep', return_value=True)         # 테스트 실행 시간을 줄이고자 함
+    # 실제 운영 코드는 while 문을 통해서 체크->지연 을 반복함
+    @patch('time.sleep', return_value=True)         # 테스트 실행 시간을 줄이고자
     def test_wait_for_db(self, ts):                 # 전달 인자로 ts 전달
         """Test waiting for db"""
         with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
